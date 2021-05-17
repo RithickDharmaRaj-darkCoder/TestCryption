19a0ab5660d0a7639fa7b2031197b76909abd57e
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-17 17:28:53 +0530

    calling decryption section is done

--- txtcryption.py
+++ txtcryption.py
@@ -272,4 +272,15 @@ def decrypt(encrypted): #Creating a User  as def decrypt()...
         else:
             decrypted += letter
     print("Decrypted Message :"+decrypted)
-decrypt(encrypted)
+
+# Getting optional answer from user...
+def yes_no():
+    yes_no_ans = input("Do you want to Decrypt the Encrypted message [Y/N] : ").upper()
+    if yes_no_ans == "Y":
+        decrypt(encrypted)
+    elif yes_no_ans == "N":
+        print("Thank You!")
+    else:
+        print("WARNING!  *Invalid Answer*")
+        yes_no()
+yes_no()
