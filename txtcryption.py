eeec3dc82110d77dee3a4fe1fadc0b34b759140f
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-19 05:26:04 +0530

    Made as calling from one file to another.

--- txtcryption.py
+++ txtcryption.py
@@ -6,8 +6,11 @@ def yes_no():
     if yes_no_ans == "Y":
         decrypt(encrypted)
     elif yes_no_ans == "N":
-        print("Thank You!")
+        print()
     else:
         print("WARNING!  *Invalid Answer*")
         yes_no()
 yes_no()
+
+#Personal Greetings...
+print("Thank You!","\n","          -darkCoder \U0001F43E")
